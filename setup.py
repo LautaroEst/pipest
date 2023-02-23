a4560f6e4b71c5600a51325bc2f06026ef09f94c
LautaroEst <lautaro.est@gmail.com>
2023-02-23 11:32:43 -0300

    back to version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pipest", # Replace with your own username
-    version="0.0.2",
+    version="0.0.1",
     author="Lautaro Estienne",
     author_email="lautaro.est@gmail.com",
     description="Python package for machine learning pipelines excecution",
