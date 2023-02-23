f5a028f8630006c4b7a5dfe00f396fc34036da1a
LautaroEst <lautaro.est@gmail.com>
2023-02-23 11:12:07 -0300

    add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pipest
+# PipEst
+
 Pipelines with predefined tasks
+
+**Note: This site is under construction**
+
+Workflow notes:
+
+* Edit the package's code and pushit to the github repo as usual
+
+* Once you are happy and build a distribution using `python -m build` in the root directory.
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pipest", # Replace with your own username
+    version="0.0.1",
+    author="Lautaro Estienne",
+    author_email="lautaro.est@gmail.com",
+    description="Python package for machine learning pipelines excecution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LautaroEst/pipest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
